80dbd10aa46647b94ce79444b679386d09efee49
Umang Patel <umang4846@gmail.com>
2022-01-03 19:36:49 +0530

    Time: 6 ms (29.83%), Space: 46.5 MB (83.42%) - LeetHub

--- find-the-town-judge/find-the-town-judge.java
+++ find-the-town-judge/find-the-town-judge.java
@@ -1,32 +1,18 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[][] arr = new int[n][n];
-        for(int i =0 ;i<trust.length;i++){
-            int e1 = trust[i][0] - 1;
-            int e2 = trust[i][1] - 1;
-            arr[e1][e2] = 1;
+        int[] arr = new int[n+1];
+        for(int[] t : trust){
+            int trustBy = t[0];
+            int trustIn = t[1];
+            arr[trustBy] = --arr[trustBy];
+            arr[trustIn] = ++arr[trustIn];
         }
-        int index = -1;
-        for(int i=0; i<n; i++){
-            boolean isFound = true;
-            for(int j=0; j<n; j++){
-                if(arr[i][j] == 1){
-                    isFound = false;
-                    break;
-                }
-            }
-            if(isFound){
-                index = i;
+        System.out.println(Arrays.toString(arr));
+        for(int i=1; i<=n;i++){
+            if(arr[i] == n-1){
+                return i;
             }
         }
-        if(index == -1){
-            return -1;
-        }
-        for(int i=0; i<n;i++){
-            if(i != index && arr[i][index] == 0){
-                return -1;
-            }
-        }
-        return index+1;
+        return -1;
     }
 }
