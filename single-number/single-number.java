bab1dd00cf993e6d44b294ddf19fc44f628b324c
Umang Patel <umang4846@gmail.com>
2022-01-05 22:33:21 +0530

    Time: 1 ms, Memory: 47.8 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,15 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i =0; i<nums.length; i++){
-            int fre = map.getOrDefault(nums[i],0);
-            map.put(nums[i],fre+1);
+        int result = 0;
+        for (int i = 0; i<nums.length; i++)
+        {
+            result ^=nums[i];
         }
-        for(int i : map.keySet()){
-            if(map.get(i) == 1){
-                return i;
-            }
-        }
-        return -1;
+	    return result;
     }
 }
