1aec81fa240f46ff2a2758d534a532b58f7466d8
Umang Patel <umang4846@gmail.com>
2022-01-06 21:00:43 +0530

    Time: 5 ms (38.87%), Space: 61.2 MB (24.91%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -1,26 +1,26 @@
 class Solution {
     int ans=0;
     public int countServers(int[][] grid) {
-        for(int row=0;row<grid.length;row++){
-            for(int col=0;col<grid[0].length;col++){
-                if((grid[row][col] == 1)){
-                    boolean isFound = false;
-                    for(int i=0;i<grid[0].length;i++){
-                        if(grid[row][i] == 1 && i != col){
-                            ans++;
-                            isFound = true;
-                            break;
-                        }
-                    }
-                    for(int i=0; !isFound && i<grid.length;i++){
-                        if(grid[i][col] == 1 && i != row){
-                            ans++;
-                            break;
-                        }
-                    }
+        int n = grid.length;
+        int m = grid[0].length;
+        int[] rowCount = new int[n];
+        int[] colCount = new int[m];
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1){
+                    rowCount[i]++;
+                    colCount[j]++;       
                 }
             }
         }
-        return ans;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1)){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
