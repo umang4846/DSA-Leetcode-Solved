d66ec7d1fce78b84d4e910003e42a31c99c50dde
Umang Patel <umang4846@gmail.com>
2022-01-11 19:42:15 +0530

    Time: 3 ms (27.71%), Space: 38.3 MB (15.66%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,17 +1,24 @@
 class Solution {
     public int mySqrt(int x) {
-         // Base cases
-        if (x == 0 || x == 1)
+        if (x==0||x==1) {
             return x;
-
-        // Starting from 1, try all numbers until
-        // i*i is greater than or equal to x.
-        long i = 1, result = 1;
-        
-        while (result <= x) {
-            i++;
-            result = i * i;
         }
-        return (int)i - 1;
-    }
+        long start  = 0;
+        long end = x;
+        int ans = 0;
+        while (start<=end){
+            long mid = start +(end -start)/2;
+            if(mid*mid == x) return (int)mid;
+            if (mid*mid>x){
+                end = mid-1;
+            }
+            else {
+                // might be possible answer
+                 ans = (int)mid;
+                start = mid +1;
+            }
+
+        }
+        return ans;
+   }
 }
