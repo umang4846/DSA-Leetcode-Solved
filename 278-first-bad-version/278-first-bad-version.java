46e0af633ed714eba8ce144fee8b23f9d1e44dca
Umang Patel <umang4846@gmail.com>
2022-01-13 22:19:15 +0530

    Time: 28 ms (12.28%), Space: 38 MB (7.41%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -5,17 +5,15 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start = 1;
         int end = n;
-        int possibleAns = -1;
         while(start <= end){
             int mid = (start + end) >>> 1;
             boolean ans = isBadVersion(mid);
             if(ans == true){
-                possibleAns = mid;
                 end = mid - 1;
             }else{
                 start = mid + 1;
             }
         }
-        return possibleAns;
+        return start;
     }
 }
