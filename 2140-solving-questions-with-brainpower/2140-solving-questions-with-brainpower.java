eb2cbfcab84aa7422a939ac456d4eaca49568adf
Umang Patel <umang4846@gmail.com>
2022-01-17 11:28:41 +0530

    Time: 4 ms (100.00%), Space: 91.8 MB (100.00%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,20 +1,14 @@
 class Solution {
     public long mostPoints(int[][] questions) {
-        long[] memo = new long[questions.length];
-        Arrays.fill(memo,-1);
-        return solve(0,questions,memo);
-    }
-    private long solve(int index, int[][] questions, long[] memo){
-        if(index >= questions.length){
-            return 0;
+        int n= questions.length; 
+        long[] dp = new long[n+1];
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+             int nx= questions[i][1]; 
+             long pick   = questions[i][0] + ( (i+1+nx<=n) ? dp[i+1+nx] : 0 );
+             long notPick = dp[i+1];
+             dp[i]= Math.max(pick,notPick);
         }
-        if(memo[index] != -1){
-            return memo[index];
-        }
-        long pick = questions[index][0] + solve(index+1+questions[index][1],questions,memo);
-        long notPick = 0 + solve(index+1,questions,memo);
-        memo[index] = Math.max(pick,notPick);
-        return memo[index];
-
+        return dp[0];
     }
 }
