590d89ac5023f836eb76ba8c7eaa60df19e2aad8
Umang Patel <umang4846@gmail.com>
2022-01-18 11:45:15 +0530

    Time: 2 ms (31.85%), Space: 49 MB (39.97%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.java
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -4,32 +4,13 @@ class Solution {
         if(n==0){
             return true;
         }
-        if(size == 1 && flowerbed[0] == 0 && n==1){
-            return true;
-        }
         for(int i=0;i<size;i++){
             if(n == 0){
                 return true;
             }
-            if(i == 0 && flowerbed[i] == 0){
-                if(i != size-1 && flowerbed[i+1] == 0){
-                    flowerbed[i] = 1;
-                    n--;
-                }else if(i == size-1){
-                    flowerbed[i] = 1;
-                    n--;
-                }else{
-                    continue;
-                }
-            }else if(i == size-1 && flowerbed[i] == 0 && flowerbed[i-1] == 0) {
+            if(flowerbed[i] == 0 && (i==0 || flowerbed[i-1] == 0) && (i==size-1 ||flowerbed[i+1] == 0)){
                 flowerbed[i] = 1;
                 n--;
-            }
-            else{
-                if(flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
-                    flowerbed[i] = 1;
-                    n--;
-                }
             }
         }
         return !(n>0);
