1f3b8c58ff7e82a9cdb9c33c0dbbd8a926601dd5
Umang Patel <umang4846@gmail.com>
2022-02-07 23:33:56 +0530

    Time: 3 ms (54.13%), Space: 42.2 MB (12.12%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -2,12 +2,11 @@ class Solution {
     public char findTheDifference(String s, String t) {
         int sSum = 0;
         for(int i=0;i<s.length();i++){
-            sSum += s.charAt(i);
+            sSum ^= s.charAt(i);
         }
-        int tSum = 0;
         for(int i=0;i<t.length();i++){
-            tSum += t.charAt(i);
+            sSum ^= t.charAt(i);
         }
-        return (char) (tSum - sSum);
+        return (char)(sSum);
     }
 }
